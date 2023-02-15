704b27a3c19f353b084dc547139db4f3adbb80dd
drloring <56944064+drloring@users.noreply.github.com>
2023-02-15 14:02:00 -0800

    Update foreground.sh

--- java-ms/java-1/foreground.sh
+++ java-ms/java-1/foreground.sh
@@ -1,11 +1,6 @@
 apt-get -y install openjdk-17-jre-headless
 
-wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-tar -xvf apache-maven-3.6.3-bin.tar.gz
-mv apache-maven-3.6.3 /opt/
-M2_HOME='/opt/apache-maven-3.6.3'
-PATH="$M2_HOME/bin:$PATH"
-export PATH
+apt-get -y install maven
 
 mkdir example
 cd example

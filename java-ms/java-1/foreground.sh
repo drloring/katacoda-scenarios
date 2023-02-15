e2fe27e02fb348eb9eb23076c5fb39e51a4e5741
drloring <56944064+drloring@users.noreply.github.com>
2023-02-15 13:59:29 -0800

    Update foreground.sh

--- java-ms/java-1/foreground.sh
+++ java-ms/java-1/foreground.sh
@@ -1,4 +1,4 @@
-apt-get -y install openjdk-17-jre-headless{{execute}}
+apt-get -y install openjdk-17-jre-headless
 
 wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar -xvf apache-maven-3.6.3-bin.tar.gz

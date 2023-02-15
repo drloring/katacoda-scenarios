010eadd99de6ba817de295352a2ca74eca2fff0c
drloring <56944064+drloring@users.noreply.github.com>
2023-02-15 13:51:44 -0800

    Update background.sh

--- java-ms/java-1/background.sh
+++ java-ms/java-1/background.sh
@@ -1,2 +1,10 @@
-cd example
+apt-get -y install `openjdk-17-jre-headless`{{execute}}
 
+wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xvf apache-maven-3.6.3-bin.tar.gz
+mv apache-maven-3.6.3 /opt/
+M2_HOME='/opt/apache-maven-3.6.3'
+PATH="$M2_HOME/bin:$PATH"
+export PATH
+
+cd example
